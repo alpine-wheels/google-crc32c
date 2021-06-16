bbc2a9e3d179d6d0d7f26f83b30e2e69a72ff52a
William Jackson <william@subtlecoolness.com>
2021-06-16 11:28:18 -0500

    Add btrfs-progs-dev to dependencies

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-apk add --no-cache gcc libffi-dev musl-dev
+apk add --no-cache btrfs-progs-dev gcc libffi-dev musl-dev
 
 # build
 pip wheel -vvv --no-deps --requirement requirements.txt
