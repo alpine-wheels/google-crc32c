a2d93b7b6e6801d2370a3ac6d1184550d2f5edc1
William Jackson <william@subtlecoolness.com>
2021-10-12 09:45:22 -0500

    Build crc32c library before building wheel

--- build.sh
+++ build.sh
@@ -3,7 +3,22 @@
 set -e
 
 # install dependencies
-# apk add --no-cache gcc libffi-dev musl-dev
+# https://github.com/googleapis/python-crc32c/issues/83#issuecomment-910515271
+apk add --no-cache cmake g++ gcc git make musl-dev
+
+git clone https://github.com/google/crc32c
+cd crc32c
+git submodule update --init --recursive
+mkdir build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCRC32C_BUILD_TESTS=no \
+    -DCRC32C_BUILD_BENCHMARKS=no \
+    -DBUILD_SHARED_LIBS=yes \
+    ..
+make all install
+cd /workspace
 
 # build
-CRC32C_PURE_PYTHON=1 pip wheel --no-deps --requirement requirements.txt
+pip wheel --no-deps --requirement requirements.txt
